e78159fe549f0c6a7bcd7eaa865330efb6c85648
sjaak <kantsjaak@gmail.com>
2021-02-17 14:29:02 +0100

    Added PHPMailer and you will get a mail now when creating an account

--- Website/pages/register.php
+++ Website/pages/register.php
@@ -13,44 +13,36 @@
     <input type="submit">
 </form>
 <?php
-//starts the session
 session_start();
 
-//requires the file
 require "../env.php";
+require "mail.php";
 
-//sets a value in the session
-$_SESSION["test"] = "test test test";
-
-//gets a value from the session
-//print_r($_SESSION["test"]);
-
-//thesekcan get the env values
-getenv("MYSQLUSERNAME");
-getenv("MYSQLPASSWORD");
-getenv("MYSQLHOST");
-getenv("MYSQLDATABASE");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = changeInput($_POST["AccName"]);
     $username = changeInput($_POST["AccUsername"]);
     $email = changeInput($_POST["AccEmail"]);
     $phone = changeInput($_POST["AccPhone"]);
     $password = changeInput($_POST["AccPassword"]);
-    $password = hash('md5', $password);
-    $password = hash('sha256', $password);
+    $password = hash('sha512', $password);
+    $verificationtoken = hash('sha256', rand(1, 1000000));
     try {
         $dbh = new PDO("mysql:host=" . getenv("MYSQLHOST") . ";" . "dbname=" . getenv("MYSQLDATABASE") . ";", getenv("MYSQLUSERNAME"), getenv("MYSQLPASSWORD"));
 
-        $stmt = $dbh->prepare("INSERT INTO `users` (name, username, email, phone, password) VALUES (:name, :username, :email, :phone, :password)");
+        $stmt = $dbh->prepare("INSERT INTO `users` (name, username, email, phone, password, verifytoken) VALUES (:name, :username, :email, :phone, :password, :verifytoken)");
         $stmt->bindParam(':name', $name);
         $stmt->bindParam(':username', $username);
         $stmt->bindParam(':email', $email);
         $stmt->bindParam(':phone', $phone);
         $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':verifytoken', $verificationtoken);
         $stmt->execute();
 
         $dbh = null;
         echo "<p id='succesMessage'>added to the database</p>";
+        sendVerificationMail($email, $name, $verificationtoken);
+        //header('Location: http://www.google.com/');
+
     } catch (PDOException $e) {
         print "Error!: " . $e->getMessage() . "<br/>";
         die();

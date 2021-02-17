24d01a3a3e452fce7822a2dd8f679a77babc4062
Paul <Paulvanderlei@hotmail.nl>
2021-02-17 14:49:10 +0100

    New spritesheet

---
+++ Sprites/Maps/OutdoorProps.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="OutdoorProps" tilewidth="32" tileheight="32" tilecount="105" columns="7">
+ <image source="../Tilesets/OutdoorProps.png" width="224" height="480"/>
+</tileset>
---
+++ Sprites/Maps/OutdoorTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="OutdoorTiles" tilewidth="32" tileheight="32" tilecount="28" columns="7">
+ <image source="../Tilesets/OutdoorTiles.png" width="224" height="128"/>
+</tileset>

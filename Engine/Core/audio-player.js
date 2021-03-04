e3d39017c7d77d4e40ebb25db770361d9b88b2cd
Paul <Paulvanderlei@hotmail.nl>
2021-03-04 11:01:34 +0100

    Audioplayer+demo

---
+++ Engine/Core/audio-player.js
@@ -0,0 +1,66 @@
+class AudioPlayer {
+    constructor() {
+        var audio = document.createElement('audio');
+        audio.id = 'audio-player';
+        this.audio = audio;
+        this.audio.volume = 1;
+        this.loop = false;
+        this.isPlaying = false;
+    }
+
+    setSource(source) {
+        this.audio.src = source;
+    }
+
+    toggleLoop() {
+        if (this.audio.loop) {
+            this.audio.loop = false;
+        } else {
+            this.audio.loop = true;
+        }
+    }
+
+    play(source = null) {
+
+        if (source) {
+            this.setSource(source);
+            this.audio.play();
+            this.isPlaying = true;
+
+        } else {
+            if (this.audio.src) {
+                if (!this.isPlaying) {
+                    this.audio.play();
+                    this.isPlaying = true;
+                } else {
+                    console.log('is playing');
+                }
+            } else {
+                console.log('no source. Use setSource() method.')
+            }
+        }
+    }
+    pause() {
+        if (this.isPlaying) {
+            this.audio.pause();
+            this.isPlaying = false;
+        }
+    }
+    audioUP() {
+        if (this.audio.volume != 1) {
+            this.audio.volume += 0.1;
+            console.log(this.audio.volume);
+        }
+
+    }
+    audioDown() {
+        if (this.audio.volume >= 0.1) {
+            this.audio.volume -= 0.1;
+            console.log(this.audio.volume);
+        }
+    }
+}
+
+(function () {
+    window.audioPlayer = new AudioPlayer();
+})();

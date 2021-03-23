dce42cb64614e10324f590f30d584c2a43d11243
jan <jangarretsen01@gmail.com>
2021-03-23 12:29:40 +0100

    test

--- Website/pages/user.php
+++ Website/pages/user.php
@@ -54,7 +54,7 @@ $userinfo = $_SESSION["userinfo"];
                         array_push($array, $entry);
                     }
 
-                    array_push($array, "{\"time\": \"now\", \"message\": \"Test Message\"}");
+                    array_push($array, json_encode("{\"time\": \"now\", \"message\": \"Test Message\"}"));
 
                     var_dump(json_encode($array));
 

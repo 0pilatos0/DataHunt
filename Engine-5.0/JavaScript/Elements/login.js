b12ab09b9cdf98d22f13b2f768c0632de50970ce
Paul <Paulvanderlei@hotmail.nl>
2021-04-29 11:57:49 +0200

    new login element

---
+++ Engine-5.0/JavaScript/Elements/login.js
@@ -0,0 +1,22 @@
+let login = document.getElementById("TogglerLogin");
+let register = document.getElementById('TogglerRegister');
+
+let loginText = document.getElementById('loginText');
+let registerText = document.getElementById('RegisterText');
+
+let loginElement = document.getElementById('loginElement');
+let registerElement = document.getElementById('RegisterElement');
+
+login.onclick = function(){
+    registerText.classList.remove('active');
+    loginText.classList.add('active');
+    loginElement.classList.remove('hidden');
+    registerElement.classList.add('hidden');
+    
+}
+register.onclick = function(){
+    loginText.classList.remove('active');
+    registerText.classList.add('active');
+    registerElement.classList.remove('hidden');
+    loginElement.classList.add('hidden');
+}

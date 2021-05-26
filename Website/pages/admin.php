c666d2b1ac1d1a90832e5ae278ffa5035a593723
jan <jangarretsen01@gmail.com>
2021-05-26 09:44:17 +0200

    updated 1 form

--- Website/pages/admin.php
+++ Website/pages/admin.php
@@ -26,19 +26,32 @@ if(isset($_GET["ban"])){
     if($_GET["ban"] === "true"){
         echo "
             <div id=\"delete-account-overlay\" onclick='removeOverlay()' class=\"overlay delete-element\">
-                
-            </div>
-            <div class=\"delete-confirm delete-element\">
-                <h3>Ban user</h3>
-                <form method='post' target='_self'>
-                <label for='id'>User ID</label>
-                <input id=\"id\" type='number' value='{$_GET["id"]}'>
-                <label for='date'>Ban until</label>
-                <input id=\"date\" type='date'>
-                <input class='hide' id=\"banBy\" value='{$userinfo["id"]}'>
-                <button class=\"btn btn-confirm\">Confirm</button>
-                <button id=\"cancel\" onclick=\"removeOverlay()\" class=\"btn btn-cancel\">Cancel</button>
-                </form>
+                <div class=\"card card-info\">
+                    <div class=\"card-header\">
+                        <h3 class=\"card-title\">Ban User</h3>
+                    </div>
+                    <form class=\"form-horizontal\ target=\"_self\">
+                        <div class=\"card-body\">
+                            <div class=\"form-group row\">
+                                <label for=\"id\" class=\"col-sm-2 col-form-label\">User ID</label>
+                                <div class=\"col-sm-10\">
+                                    <input class=\"form-control\" id=\"id\" type='number' value='{$_GET["id"]}'>
+                                </div>
+                            </div>
+                            <div class=\"form-group row\">
+                                <label for=\"date\" class=\"col-sm-2 col-form-label\">Ban Until</label>
+                                 <div class=\"col-sm-10\">
+                                    <input type=\"date\" class=\"form-control\" id=\"date\">
+                                 </div>
+                            </div>
+                            <input class='hide' id=\"banBy\" value='{$userinfo["id"]}'>
+                        </div>
+                        <div class=\"card-footer\">
+                            <button type=\"submit\" class=\"btn btn-info\">Sign in</button>
+                            <button type=\"submit\" class=\"btn btn-default float-right\">Cancel</button>
+                        </div>
+                    </form>
+                </div>
             </div>";
     }
 }

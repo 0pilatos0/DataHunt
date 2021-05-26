c4155af945ed1d690aa824e17c67773b841dad81
jan <jangarretsen01@gmail.com>
2021-05-26 11:46:38 +0200

    removed search from the table

--- Website/pages/admin.php
+++ Website/pages/admin.php
@@ -62,16 +62,6 @@ if(isset($_POST["date"]) && !empty($_POST["date"])){
         <div id="usertable" class="card">
             <div class="card-header">
                 <h3 class="card-title">User Table</h3>
-
-                <div class="card-tools">
-                    <div class="input-group input-group-sm" style="width: 150px;">
-                        <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-
-                        <div class="input-group-append">
-                            <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
-                        </div>
-                    </div>
-                </div>
             </div>
             <!-- /.card-header -->
             <div class="card-body table-responsive p-0">

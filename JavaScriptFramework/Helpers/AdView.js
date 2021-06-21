10cf643fd19c9744a493a1a4864b730e1f8f3d31
EmileDeGamer <47303713+EmileDeGamer@users.noreply.github.com>
2021-06-21 11:10:06 +0200

    Added adview helper

---
+++ JavaScriptFramework/Helpers/AdView.js
@@ -0,0 +1,11 @@
+const HTMLFileLoader = require("../Classes/HTMLFileLoader")
+
+module.exports = class Modal{
+    constructor({title, body, confirm, session}){
+        let modalPage = new HTMLFileLoader(`${__dirname}/../Elements/adView.html`)
+        modalPage.vars.TITLE = title
+        modalPage.vars.BODY = body
+        modalPage.vars.CONFIRM = confirm
+        session.modal = modalPage.data
+    }
+}
